41edd473a5585ed17b14ed50c061e6a01d6da0d5
msharp9 <msharp9@outlook.com>
2017-02-18 22:46:27 -0700

    Completed project objectives

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,5 +1,18 @@
 $(function() {
-
-  // your code will go here
+  $.ajax('https://www.codeschool.com/users/msharp9.json', {
+    dataType: 'jsonp'
+    ,success: function(result) {
+      //console.log(result);
+      var badges = $.map(result.courses.completed, function(completedCourse,i){
+        var div = $('<div></div>');
+        div.addClass('course');
+        $('<h3>'+completedCourse.title+'</h3>').appendTo(div);
+        $('<img src='+completedCourse.badge+'></img>').appendTo(div);
+        $('<a href='+completedCourse.url+' target=_blank class="btn btn-primary">See Course</a>').appendTo(div);
+        return div
+      });
+      $('#badges').detach().html(badges).appendTo($('.container'))
+    }
+  });
 
 });
